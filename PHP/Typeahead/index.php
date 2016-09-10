7985c7f08c808d4c4bd9ad536c20bdf0b93dc7ed
Monique Szpak <mszpak@zenlan.com>
2016-09-10 14:42:20 +0100

    Adds Typeahead snippet

---
+++ PHP/Typeahead/index.php
@@ -0,0 +1,28 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Typeahead.js</title>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/jquery.typeahead.min.css">
+    <link rel="stylesheet" href="css/examples.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css">
+    <style>
+      body {font-family:sans-serif;}
+      div {padding:10px;}
+    </style>
+  </head>
+  <body>
+    <div>
+      <form action="" method="POST">
+        <p><input type="text" id="tag" class="typeahead" value="" size="128" placeholder="Start typing..."/></p>
+      </form>
+    </div>
+  </body>
+  <script src="//code.jquery.com/jquery-1.9.1.min.js"></script>
+  <script src="//code.jquery.com/ui/1.9.1/jquery-ui.min.js"></script>
+  <script src="js/jquery.typeahead.min.js"></script>
+  <script src="js/typeahead.bundle.min.js"></script>
+  <script src="js/zenlan.js"></script>
+</html>
